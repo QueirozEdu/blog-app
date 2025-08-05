e4cdd250c351fbfa041336031922ea8b0690a700
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-05 20:56:16 -0300

    refactor: create post featured individual component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,49 +3,16 @@ import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsLists";
 import SpinLoader from "@/components/SpinLoader";
 import { Suspense } from "react";
-import { PostHeading } from "@/components/PostHeadingindex";
-import { PostCoverImage } from "@/components/PostCoverImage";
+import { PostFeatured } from "@/components/PostFeatured";
 
 export default async function HomePage() {
     return (
         <Container>
             <Header />
 
-            <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-                <PostCoverImage
-                    linkProps={{
-                        href: "/post/asdfasdf",
-                    }}
-                    imageProps={{
-                        width: 1200,
-                        height: 720,
-                        src: "/images/bryen_9.png",
-                        alt: "Alt da imagem",
-                        priority: true,
-                    }}
-                />
-
-                <div className="flex flex-col gap-4 sm:justify-center">
-                    <time
-                        className="text-slate-600 block text-sm/tight"
-                        dateTime="2025/07/30"
-                    >
-                        30/07/2025 09:41
-                    </time>
-
-                    <PostHeading as="h1" url="#">
-                        Quibusdam culpa laudantium, at incidunt nihil ac
-                    </PostHeading>
-
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Nesciunt harum atque minima nostrum temporibus
-                        repudiandae quasi quibusdam culpa laudantium, at
-                        incidunt nihil accusamus rem dolores enim, libero magni
-                        id impedit?
-                    </p>
-                </div>
-            </section>
+            <Suspense fallback={<SpinLoader />}>
+                <PostFeatured />
+            </Suspense>
 
             <Suspense fallback={<SpinLoader />}>
                 <PostsList />
