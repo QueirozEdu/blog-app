365b4c9d7a23328f980005b271acf5e66697d9a0
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-05 21:30:39 -0300

    add: title with formated date and time

--- src/components/PostsLists/index.tsx
+++ src/components/PostsLists/index.tsx
@@ -1,6 +1,7 @@
 import { postRepository } from "@/repositories/post";
 import { PostCoverImage } from "../PostCoverImage";
 import { PostHeading } from "../PostHeadingindex";
+import { formatDateTime, formatDistanceToNow } from "@/utils/format-datetime";
 
 export async function PostsList() {
     const posts = await postRepository.findAll();
@@ -27,8 +28,9 @@ export async function PostsList() {
                             <time
                                 className="text-slate-600 block text-sm/tight"
                                 dateTime={post.createdAt}
+                                title={formatDateTime(post.createdAt)}
                             >
-                                {post.createdAt}
+                                {formatDistanceToNow(post.createdAt)}
                             </time>
 
                             <PostHeading as="h2" url={postLink}>
--- src/utils/format-datetime.ts
+++ src/utils/format-datetime.ts
@@ -1,4 +1,7 @@
-import { format, formatDistanceToNow } from "date-fns";
+import {
+    format,
+    formatDistanceToNow as dateFnsFormatDistanceToNow,
+} from "date-fns";
 import { enUS, pt, ptBR } from "date-fns/locale";
 
 export function formatDateTime(rawDate: string): string {
@@ -9,10 +12,10 @@ export function formatDateTime(rawDate: string): string {
     });
 }
 
-export function formatRelativeDate(rawDate: string): string {
+export function formatDistanceToNow(rawDate: string): string {
     const date = new Date(rawDate);
 
-    return formatDistanceToNow(date, {
+    return dateFnsFormatDistanceToNow(date, {
         locale: enUS,
         addSuffix: true,
     });
@@ -21,4 +24,4 @@ export function formatRelativeDate(rawDate: string): string {
 const rawDate = "2025-08-06T00:06:35.545Z";
 console.log(rawDate);
 console.log(formatDateTime(rawDate));
-console.log(formatRelativeDate(rawDate));
+console.log(formatDistanceToNow(rawDate));
