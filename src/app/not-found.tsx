50700a8c0d0799c3fc7eb16bf68fb8207474a128
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-08 23:01:47 -0300

    add title for page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from "clsx";
 
 export default function NotFoundPage() {
     return (
-        <div
-            className={clsx(
-                "min-h-[320px] bg-slate-900 text-slate-100",
-                "mb-16 p-8 rounded-xl",
-                "flex items-center justify-center",
-                "text-center"
-            )}
-        >
-            <div>
-                <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-                <p>The page you are looking for does not exist.</p>
+        <>
+            <title>Page not found</title>
+            <div
+                className={clsx(
+                    "min-h-[320px] bg-slate-900 text-slate-100",
+                    "mb-16 p-8 rounded-xl",
+                    "flex items-center justify-center",
+                    "text-center"
+                )}
+            >
+                <div>
+                    <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+                    <p>The page you are looking for does not exist.</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
