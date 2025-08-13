479d3f3e32d1015bc76bfb69b6693089f4cae538
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-13 15:11:43 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import { PostFeatured } from "@/components/PostFeatured";
 
 export default async function HomePage() {
     return (
-        <>
-            <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
-                <PostFeatured />
-                <PostsList />
-            </Suspense>
-        </>
+        <Suspense fallback={<SpinLoader className="min-h-20 mb-16" />}>
+            <PostFeatured />
+            <PostsList />
+        </Suspense>
     );
 }
