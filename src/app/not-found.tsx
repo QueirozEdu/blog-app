4beaf1dec623b1f0c56c7f45c3a4e1e7c0187d96
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-13 13:50:33 -0300

    add better error treatment

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,13 @@
-import clsx from "clsx";
+import ErrorMessage from "@/components/ErrorMessage";
 
 export default function NotFoundPage() {
     return (
         <>
-            <title>Page not found</title>
-            <div
-                className={clsx(
-                    "min-h-[320px] bg-slate-900 text-slate-100",
-                    "mb-16 p-8 rounded-xl",
-                    "flex items-center justify-center",
-                    "text-center"
-                )}
-            >
-                <div>
-                    <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-                    <p>The page you are looking for does not exist.</p>
-                </div>
-            </div>
+            <ErrorMessage
+                pageTitle="Page not found"
+                contentTitle="404"
+                content="The page you are looking for does not exist."
+            />
         </>
     );
 }
