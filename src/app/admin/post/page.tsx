69b0e9b0ac30c08557f7f707d1653f50f0695b73
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-19 15:23:40 -0300

    refactor: separate queries into admin and public

--- src/app/admin/post/page.tsx
+++ src/app/admin/post/page.tsx
@@ -1,5 +1,20 @@
+import { findAllPostsAdmin } from "@/lib/post/queries/admin";
+import { Metadata } from "next";
+
 export const dynamic = "force-dynamic";
 
+export const metadata: Metadata = {
+    title: "Post Admin",
+};
+
 export default async function AdminPostPage() {
-    return <div className="py-16 text-6xl">Admin Post Page</div>;
+    const posts = await findAllPostsAdmin();
+
+    return (
+        <div className="py-16 text-xl">
+            {posts.map((post) => {
+                return <p key={post.id}>{post.title}</p>;
+            })}
+        </div>
+    );
 }
