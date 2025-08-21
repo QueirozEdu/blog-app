2797477a2da376cc7901ac995c597215e4db875e
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-20 21:15:11 -0300

    add: styling for post list admin page

--- src/components/PostListAmin/index.tsx
+++ src/components/PostListAmin/index.tsx
@@ -1,12 +1,47 @@
 import { findAllPostsAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import { Trash2Icon } from "lucide-react";
+import Link from "next/link";
 
 export default async function PostsListAdmin() {
     const posts = await findAllPostsAdmin();
 
     return (
-        <div className="py-16 text-xl">
+        <div className="mb-16">
             {posts.map((post) => {
-                return <p key={post.id}>{post.title}</p>;
+                return (
+                    <div
+                        className={clsx(
+                            "py-2 px-2",
+                            !post.published && "bg-slate-300",
+                            "flex gap-2 items-center justify-between"
+                        )}
+                        key={post.id}
+                    >
+                        <Link href={`/admin/post/${post.id}`}>
+                            {post.title}
+                        </Link>
+
+                        {!post.published && (
+                            <span className="text-xs text-slate-600 italic">
+                                {" "}
+                                Not published
+                            </span>
+                        )}
+
+                        <button
+                            className={clsx(
+                                "text-red-400 cursor-pointer transition",
+                                "[&_svg]:w-4 [&_svg]:h-4",
+                                "hover:scale-130 hover:text-red-700"
+                            )}
+                            aria-label={`Delete post: ${post.title}`}
+                            title={`Delete post: ${post.title}`}
+                        >
+                            <Trash2Icon size={18} />
+                        </button>
+                    </div>
+                );
             })}
         </div>
     );
