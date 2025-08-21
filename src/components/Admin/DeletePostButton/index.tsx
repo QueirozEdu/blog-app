dfe94b3b84f9e967e63c5c910f49c84b73b539d1
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-21 16:08:16 -0300

    add delete button action

--- src/components/Admin/DeletePostButton/index.tsx
+++ src/components/Admin/DeletePostButton/index.tsx
@@ -2,6 +2,8 @@
 
 import clsx from "clsx";
 import { Trash2Icon } from "lucide-react";
+import { deletePostAction } from "@/actions/post/delete-post-action";
+import { useTransition } from "react";
 
 type DeletePostButtonProps = {
     id: string;
@@ -9,19 +11,27 @@ type DeletePostButtonProps = {
 };
 
 export function DeletePostButton({ id, title }: DeletePostButtonProps) {
+    const [isPending, startTransition] = useTransition();
+
     function handleClick() {
-        alert("button clicked " + id);
+        if (!confirm("Are you sure? ")) return;
+        startTransition(async () => {
+            const result = await deletePostAction(id);
+            alert(`The result is ${result}`);
+        });
     }
     return (
         <button
             className={clsx(
                 "text-red-400 cursor-pointer transition",
                 "[&_svg]:w-4 [&_svg]:h-4",
-                "hover:scale-130 hover:text-red-700"
+                "hover:scale-130 hover:text-red-700",
+                "disabled:text-slate-600 disabled:cursor-progress"
             )}
             aria-label={`Delete post: ${title}`}
             title={`Delete post: ${title}`}
             onClick={handleClick}
+            disabled={isPending}
         >
             <Trash2Icon size={18} />
         </button>
