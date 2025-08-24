602c95dcbb1983d074f9c7d742465312209d7d30
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-23 21:56:01 -0300

    add logic to open and close nav bar on small devices

--- src/components/Admin/MenuAdmin/index.tsx
+++ src/components/Admin/MenuAdmin/index.tsx
@@ -1,25 +1,53 @@
+"use client";
+
 import clsx from "clsx";
-import { FileTextIcon, HouseIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 export function MenuAdmin() {
+    const [isOpen, setIsOpen] = useState(false);
+
     const navClasses = clsx(
         "bg-slate-900 text-slate-100 rounded-lg",
         "flex flex-col mb-8",
-        "sm:flex-row sm:flex-wrap"
-        // "h-10"
-        // "overflow-hidden"
+        "sm:flex-row sm:flex-wrap",
+        !isOpen && "h-10",
+        !isOpen && "overflow-hidden",
+        "sm:overflow-visible sm:h-auto"
     );
     const linkClasses = clsx(
         "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
-        "flex items-center justify-start gap-2",
+        "flex items-center justify-start gap-2 cursor-pointer",
         "transition hover:bg-slate-800 rounded-lg",
         "h-10",
         "shrink-0"
     );
 
+    const openCloseBtnClasses = clsx(
+        linkClasses,
+        "text-blue-200 italic sm:hidden"
+    );
+
     return (
         <nav className={navClasses}>
+            <button
+                onClick={() => setIsOpen((s) => !s)}
+                className={openCloseBtnClasses}
+            >
+                {!isOpen && (
+                    <>
+                        <MenuIcon />
+                        Menu
+                    </>
+                )}
+                {isOpen && (
+                    <>
+                        <CircleXIcon />
+                        CLose
+                    </>
+                )}
+            </button>
             <a className={linkClasses} href="/" target="_blank">
                 <HouseIcon />
                 Home
