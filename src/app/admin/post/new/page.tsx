dabe5f3443fe331ab5183cd121157baa6c3dac7b
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-24 22:31:14 -0300

    add button component with variants

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,19 @@
+import { Button } from "@/components/Button";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
-    return <div className="py-16 text-6xl">Admin Post New Page</div>;
+    return (
+        <div className="py-16 flex gap-4 flex-wrap">
+            <Button variant="default" size="sm">
+                Confirm
+            </Button>
+            <Button variant="ghost" size="md">
+                Test
+            </Button>
+            <Button variant="danger" size="lg">
+                Testestestes
+            </Button>
+        </div>
+    );
 }
