46a27381e84b1ef03950b15eb20896d7c2650f99
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-24 22:52:27 -0300

    add size and styling differing for button component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,19 +1,25 @@
 import { Button } from "@/components/Button";
+import { BugIcon } from "lucide-react";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
     return (
-        <div className="py-16 flex gap-4 flex-wrap">
-            <Button variant="default" size="sm">
-                Confirm
-            </Button>
-            <Button variant="ghost" size="md">
-                Test
-            </Button>
-            <Button variant="danger" size="lg">
-                Testestestes
-            </Button>
+        <div>
+            <div className="py-16 flex gap-4 flex-wrap items-center">
+                <Button variant="default" size="sm">
+                    <BugIcon />
+                    Confirm
+                </Button>
+                <Button variant="ghost" size="md">
+                    <BugIcon />
+                    Confirm
+                </Button>
+                <Button variant="danger" size="lg">
+                    <BugIcon />
+                    Confirm
+                </Button>
+            </div>
         </div>
     );
 }
