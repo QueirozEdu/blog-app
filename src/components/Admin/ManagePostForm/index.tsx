eac76b6dee1355608a0a72372ba509e5f67907b7
Eduardo Queiroz <eduhmq@gmail.com>
2025-08-29 15:33:02 -0300

    fix: edit/create post form

--- src/components/Admin/ManagePostForm/index.tsx
+++ src/components/Admin/ManagePostForm/index.tsx
@@ -12,39 +12,74 @@ export function ManagePostForm() {
     return (
         <form action="" className="mb-16">
             <div className="flex flex-col gap-6">
-                <InputText labelText="Name" placeholder="Enter your name" />
-                <InputText
-                    labelText="Lastname"
-                    placeholder="Enter your lastname"
-                />
+                {/* id: string; auto
+    title: string; auto
+    slug: string;
+    excerpt: string;
+    content: string;
+    UPLOADER
+    coverImageUrl: string;
+    published: boolean;
+    createdAt: string; auto
+    updatedAt: string; auto
+    author: string; */}
 
                 <InputText
-                    disabled
-                    labelText="Place Holder"
-                    placeholder="This is a place holder"
-                />
-                <ImageUploder />
-                <InputText
-                    disabled
-                    labelText="Default Value"
-                    placeholder="This is a place holder"
-                    defaultValue="This is a default value"
-                />
-                <InputText
-                    labelText="Read only"
-                    placeholder="This is a place holder"
-                    defaultValue="This is a default value"
+                    labelText="ID"
+                    name="id"
+                    placeholder="ID generated automatically"
+                    type="text"
+                    defaultValue={" "}
                     readOnly
                 />
-                <InputCheckBox labelText="Checkbox" />
-
+                <InputText
+                    labelText="Slug"
+                    name="slug"
+                    placeholder="Slug generated automatically"
+                    type="text"
+                    defaultValue={" "}
+                    readOnly
+                />
+                <InputText
+                    labelText="Author"
+                    name="author"
+                    placeholder="Enter the author's name"
+                    type="text"
+                />
+                <InputText
+                    labelText="Title"
+                    name="title"
+                    placeholder="Your post title"
+                    type="text"
+                />
+                <InputText
+                    labelText="Excerpt"
+                    name="excerpt"
+                    placeholder="Enter excerpt"
+                    type="text"
+                />
                 <MarkdownEditor
-                    labelText="content"
-                    disabled={false}
-                    textAreaName="content"
+                    labelText="Content"
                     value={contentValue}
                     setValue={setContentValue}
-                ></MarkdownEditor>
+                    textAreaName="content"
+                    disabled={false}
+                />
+
+                <ImageUploder />
+
+                <InputText
+                    labelText="Cover image URL"
+                    name="coverImageUrl"
+                    placeholder="Enter the image URL"
+                    type="text"
+                />
+
+                <InputCheckBox
+                    labelText="Publish?"
+                    name="published"
+                    type="checkbox"
+                />
 
                 <div className="mt-4">
                     <Button type="submit">Send</Button>
