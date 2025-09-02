c0702531ff476308d3a9d5cea2b535b87efa7c4a
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-02 18:56:07 -0300

    habilitator add functions to generate and compare hashed passwords in base64

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -10,6 +10,20 @@ type LoginActionState = {
 export async function loginAction(state: LoginActionState, formData: FormData) {
     await asyncDelay(3000);
 
+    if (!(formData instanceof FormData)) {
+        return {
+            username: "",
+            error: "Invalid credentials",
+        };
+    }
+
+    //Data the user entered in the form
+    const username = formData.get("username")?.toString() || "";
+    const password = formData.get("password")?.toString() || "";
+
+    const isUsernameValid = username === process.env.LOGIN_USER;
+    const isPassowrdValid = username === process.env.LOGIN_USER;
+
     return {
         username: "",
         error: "Error!!",
